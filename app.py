d47dd6b1e5bb8a85447568b270d85c3e859e77a2
Mehmet Serdar EREN <seninmailin@example.com>
2026-02-21 14:35:48 +0300

    app.py

--- app.py
+++ app.py
@@ -926,4 +926,4 @@ with app.app_context():
     db.create_all()
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True) # debug=True ekledik
+    app.run(host="0.0.0.0", port=port) # debug=True ekledik
