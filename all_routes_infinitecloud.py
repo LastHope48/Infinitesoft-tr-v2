8227aa94d600fba97937ee0412ba2d89927757a5
Mehmet Serdar EREN <seninmailin@example.com>
2026-03-16 15:33:49 +0300

    v5.5.3

--- all_routes_infinitecloud.py
+++ all_routes_infinitecloud.py
@@ -14,7 +14,7 @@ from datetime import datetime, timedelta
 import io,zipfile
 from sqlalchemy import func,text
 from all_classes import db
-bp = Blueprint('infinitecloud', __name__,subdomain="infinitecloud")
+bp = Blueprint('infinitecloud', __name__,subdomain="infinitecloud",static_folder="static",static_url_path="/static")
 R2_BUCKET="infinitecloud"
 MAX_STORAGE = 10 * 1024 * 1024 * 1024
 PYANYWHERE_UPLOAD_URL = "https://wf5528.pythonanywhere.com/upload"
